24bc94e876577445be11bf74ebb8dd983d1d3877
DmytroZozuliak <dmytro.zozuliak@gmail.com>
2022-08-11 16:26:36 +0300

    fix: image webp

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,7 +11,7 @@ const Header = ({ scrollToGetRequestElem, scrollToPostRequestElem }: IProps) => 
   return (
     <Box component="header" className="container">
       <Box className={classes.header}>
-        <img src={Logo} alt="logo" />
+        <img src={Logo} alt="logo" width={104} height={26} />
         <Box className={classes.buttons}>
           <Button variant="contained" color="primary" onClick={scrollToGetRequestElem}>
             Users
